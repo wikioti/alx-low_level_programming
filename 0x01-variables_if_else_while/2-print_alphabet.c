4df642f183401092567e23556987e187e7e8e5e0
wikioti <wikioti@gmail.com>
2023-07-14 15:44:34 -0700

    Alphabetics

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <ctype.h>
+
 /**
  * main - Entry point
  *
  * Retun: Always 0 (success)
  */
+
 int main(void)
 {
 	int lowerCase = 'a';
-	while lowerCase <= 'z')
+	while (lowerCase <= 'z')
 	{
 		putchar(lowerCase);
 		lowerCase += 1;
