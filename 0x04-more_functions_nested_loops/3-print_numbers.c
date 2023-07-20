59989c064d515ce7a886846fa5f9dc629a736bbd
wikioti <wikioti@gmail.com>
2023-07-20 04:14:55 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_most_numbers - prints numbersd from 0 to 9, excluding 2 and 4.
+ *
+ * Return: void.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
+			_putchar(i);
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * print_most_numbers - Prints the numbers from 0-9.
+ * Description: print the numbers except for 2 and 4.
+ * Return: The numbers since 0 up to 9.
  */
 void print_most_numbers(void)
 {
-	int num;
+	int w;
 
 	for (num = 0; num <= 9; num++)
 	{
