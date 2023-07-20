140a2830bf03b037895b334cdbb7ca24fd346367
wikioti <wikioti@gmail.com>
2023-07-20 09:45:13 -0700

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,28 +2,30 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer
+ * @a: first nteger
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c && b > a)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest);
+}}
