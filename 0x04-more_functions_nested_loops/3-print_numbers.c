e555ceba2933200801850ba0ba53f0cd0c51d939
wikioti <wikioti@gmail.com>
2023-07-20 04:49:20 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,11 @@
  *
  * Return: void.
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
-		if (i != '2' && i != '4')
-			_putchar(i);
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
